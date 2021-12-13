eb3f946e31a5920bf9b41f8cebe4ea0e7486c03a
John Zúñiga <jzuniga@chattigo.com>
2021-12-13 15:41:56 -0300

    Api Rest, rutas principales creadas

---
+++ src/main/java/com/openwebinars/rest/model/ProductoRepositorio.java
@@ -0,0 +1,7 @@
+package com.openwebinars.rest.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
+
+}
